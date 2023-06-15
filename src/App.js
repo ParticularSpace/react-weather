588bf342ac751e148360e6517d089af0bddb4cf7
Sam Jones <samejones2018@gmail.com>
2023-06-15 16:38:25 -0700

    Serve static files in production

--- src/App.js
+++ src/App.js
@@ -9,15 +9,18 @@ function App() {
   const [weatherData, setWeatherData] = useState(null);
   const [forecastData, setForecastData] = useState(null); // Add a state variable for the forecast data
 
+  const apiKey = process.env.OPEN_WEATHER_MAP_API_KEY;
+  console.log(apiKey);
+
   useEffect(() => {
     if (city) {
-      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`)
         .then((response) => response.json())
         .then((data) => setWeatherData(data))
         .catch((error) => console.error('Error:', error));
 
       // Fetch the forecast data
-      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${process.env.REACT_APP_API_KEY}`)
+      fetch(`https://api.openweathermap.org/data/2.5/forecast?q=${city}&units=imperial&appid=${apiKey}`)
         .then((response) => response.json())
         .then((data) => setForecastData(data))
         .catch((error) => console.error('Error:', error));
